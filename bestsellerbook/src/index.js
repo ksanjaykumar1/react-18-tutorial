65bad93b1b5c4e9fad6995ea06e8f2b033a25794
Sanjay <ksanjayk@protonmail.com>
2023-11-08 15:07:25 +0530

    inital best selling book website

---
+++ bestsellerbook/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const BookList = ()=> {
+    return <section>
+        <Book/>
+    </section>
+}
+const Book = ()=> {
+    return <article>
+        <Image/>
+        <Title/>
+        <Author/>
+    </article>
+}
+const Image = ()=> <h2>image placeholder</h2>
+const Title = ()=> <h2>Book Title</h2>
+const Author = ()=> <h4>Author</h4>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<BookList/>);
