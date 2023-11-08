9169c684679be27ee943fae87924b512f6d506a1
Sanjay <ksanjayk@protonmail.com>
2023-11-08 18:14:20 +0530

    passing object value using spread operator

--- bestsellerbook/src/index.js
+++ bestsellerbook/src/index.js
@@ -3,29 +3,26 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 
-const firstBook = {
-  title: 'The Stranger',
-  author: 'Albert Camus',
-  img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1590930002i/49552.jpg',
-};
-const secondBook = {
-  title: 'Crime and Punishment ',
-  author: 'Fyodor Dostoevsky',
-  img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1382846449i/7144.jpg',
-};
+const books = [
+  {
+    title: 'The Stranger',
+    author: 'Albert Camus',
+    img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1590930002i/49552.jpg',
+    id: 1,
+  },
+  {
+    title: 'Crime and Punishment ',
+    author: 'Fyodor Dostoevsky',
+    img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1382846449i/7144.jpg',
+    id: 2,
+  },
+];
 const BookList = () => {
   return (
     <section className='booklist'>
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      />
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
+      {books.map((book) => (
+        <Book {...book} key={book.id} />
+      ))}
     </section>
   );
 };
@@ -34,7 +31,14 @@ const Book = ({ title, author, img }) => {
     <article className='book'>
       <img alt={title} src={img} />
       <h2>{title}</h2>
-      <h4>{ author}</h4>
+      <h4>{author}</h4>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis
+        quaerat reprehenderit minima facere, eos officia omnis, sapiente sed
+        neque eligendi, nulla ea illum saepe distinctio aspernatur soluta.
+        Molestiae, ipsa voluptatibus.
+      </p>
+      <button>click me</button>
     </article>
   );
 };
