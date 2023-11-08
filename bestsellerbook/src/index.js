a51397ed9e488ab0a4faea1fd90ae66f18957604
Sanjay <ksanjayk@protonmail.com>
2023-11-08 16:52:54 +0530

    props

--- bestsellerbook/src/index.js
+++ bestsellerbook/src/index.js
@@ -3,26 +3,38 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 
+const firstBook = {
+  title: 'The Stranger',
+  author: 'Albert Camus',
+  img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1590930002i/49552.jpg',
+};
+const secondBook = {
+  title: 'Crime and Punishment ',
+  author: 'Fyodor Dostoevsky',
+  img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1382846449i/7144.jpg',
+};
 const BookList = () => {
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
     </section>
   );
 };
-const Book = () => {
-  const title = 'The Stranger';
-  const author = 'Albert Camus';
-  const img =
-    'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1590930002i/49552.jpg';
+const Book = ({ title, author, img }) => {
   return (
     <article className='book'>
       <img alt={title} src={img} />
       <h2>{title}</h2>
-      <h4>{author}</h4>
+      <h4>{ author}</h4>
     </article>
   );
 };
