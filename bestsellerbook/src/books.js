9330e0090ad5bd28156e45ab1fa2d3a8c7d41504
Sanjay <ksanjayk@protonmail.com>
2023-11-13 16:46:48 +0530

    book added

--- bestsellerbook/src/books.js
+++ bestsellerbook/src/books.js
@@ -11,4 +11,10 @@ export const books = [
     img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1382846449i/7144.jpg',
     id: 2,
   },
+  {
+    title: 'Nausea ',
+    author: 'Jean-Paul Sartre',
+    img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1377674928i/298275.jpg',
+    id: 3,
+  },
 ];
