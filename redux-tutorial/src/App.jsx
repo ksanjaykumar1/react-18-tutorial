b7275be5561aaad31e006b329714aac9d9eb429d
Sanjay <ksanjayk@protonmail.com>
2023-11-17 15:07:44 +0530

    modal functionality

--- redux-tutorial/src/App.jsx
+++ redux-tutorial/src/App.jsx
@@ -3,15 +3,18 @@ import CartContainer from './components/CartContainer';
 import Navbar from './components/Navbar';
 import { useDispatch, useSelector } from 'react-redux';
 import { calculateTotals } from './features/cart/cartSlice';
+import Modal from './components/Modal';
 
 function App() {
   const dispatch = useDispatch();
   const { cartItems } = useSelector((state) => state.cart);
+  const { isOpen } = useSelector((state) => state.modal);
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
   return (
     <main>
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
